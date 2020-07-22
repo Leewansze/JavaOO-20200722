a0dfdc122e43b3c4956a810e3ac16b76fc73b2c5
ALICE W S LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIAL3@OOCL.COM>
2020-07-22 16:52:30 +0800

    [practice02][Alice]feature:realize the person&student class

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,18 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int Klass;
+    public Student(String name, int age, int classNum) {
+        super(name, age);
+        this.Klass = classNum;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public String introduce(){
+        String introduce = "I am a Student. I am at Class " + this.Klass + ".";
+        return introduce;
+    }
 }
