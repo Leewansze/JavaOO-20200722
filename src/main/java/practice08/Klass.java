a5c0bef527355c016a244ad5a5ce6e9274e2eb76
ALICE W S LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIAL3@OOCL.COM>
2020-07-23 02:00:23 +0800

    [practice08][Alice]feature: add assignLeader and getDisplayName function in Klass

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,35 @@
 package practice08;
 
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+    public Klass() {
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        StringBuilder strClassName = new StringBuilder();
+        if(this.number == null){
+            strClassName.append("No Class");
+        }else{
+            strClassName.append("Class ").append(this.number);
+        }
+        return strClassName.toString();
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
