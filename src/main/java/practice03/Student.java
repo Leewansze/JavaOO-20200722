1f64c0f7249c06ab96d8a4d301db58b1a0d2948c
ALICE W S LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIAL3@OOCL.COM>
2020-07-22 17:38:08 +0800

    [practice03][Alice]feature:complete the implementation of the class

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,20 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    private int Klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = "I am a Student. I am at Class " + this.Klass + ".";
+        return introduce;
+    }
 }
