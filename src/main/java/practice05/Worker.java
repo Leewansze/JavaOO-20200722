ab1476bfd3172c49db7b5cef8dfac4ce5d629cae
ALICE W S LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIAL3@OOCL.COM>
2020-07-23 00:14:14 +0800

    [practice05][Alice]feature: Add constructor and introduce function

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,15 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        StringBuilder strIntroduce = new StringBuilder();
+        strIntroduce.append(super.introduce()).append(" I am a Worker. I have a job.");
+        return strIntroduce.toString();
+    }
 }
+
