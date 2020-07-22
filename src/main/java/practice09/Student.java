c919176b7d31375d1b56e9577e4e65631a6db71e
ALICE W S LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIAL3@OOCL.COM>
2020-07-23 02:33:21 +0800

    [practice09][Alice]feature :implement the class Student

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,30 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+        this.klass = new Klass();
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce;
+        if(this.equals(this.klass.getLeader())){
+            introduce = super.introduce() + " I am a Student. I am Leader of " + this.klass.getDisplayName()+ ".";
+        }else{
+            introduce = super.introduce() + " I am a Student. I am at " + this.klass.getDisplayName()+ ".";
+        }
+        return introduce;
+    }
 }
