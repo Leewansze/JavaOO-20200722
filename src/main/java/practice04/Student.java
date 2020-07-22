23e83d8b29e869408d65cd4af1395751301d269d
ALICE W S LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIAL3@OOCL.COM>
2020-07-22 18:25:47 +0800

    [practice04][Alice]feature: Add basiceIntrouce in Person and undate introduce in Student and Worker

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,20 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+    private int Klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = "My name is " + this.getName() +". I am " + this.getAge() + " years old. I am a Student. I am at Class " + this.Klass + ".";
+        return introduce;
+    }
 }
